22368f06bb275c076912029ed44f1156b34607d7
havidza <zaenialmuttaqien@gmail.com>
2022-07-13 11:05:47 +0700

    new midtrans callback function

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,13 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-
-
 //Socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
+
+//midtrans routes
+Route::get('payment/success', [UserController::class, 'midtransCallback']);
+Route::post('payment/success', [UserController::class, 'midtransCallback']);
 
 Route::middleware(['auth'])->group(function () {
     //checkout route
